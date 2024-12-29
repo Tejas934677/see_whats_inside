e70b7d0071b3659c4106e5a1da381731ee5850ae
Tejas gowda.S <tgowda934@gmail.com>
2024-12-29 22:00:54 +0530

    click

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os 
+def show(f): 
+    try :
+        for i,o,u in os.walk(f): 
+            print("directory:" + i)
+            for a in o : 
+                print("subfolders:"+a)
+            for b in u : 
 
-for i,o,u in os.walk("C:/Users/nn194/Desktop"): 
-    print("directory:" + i)
-    for a in o : 
-        print("subfolders:"+a)
-    for b in u : 
+                print("file : "+b)
+    except FileNotFoundError: 
+        pass # include in the reason of complicated 
+    else : 
+        print(f"your system not incudes this{f} file")
+a = "."  # replace this . with your desired file name or else it does for all system includes.
+show(a)
 
-        print("file : "+b)
+    
